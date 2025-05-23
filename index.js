52fc6dcc5823f7700fd33fa28b14b2fb6c976851
aniketdebnath <rkstaraniket34@gmail.com>
2025-05-23 13:42:25 +1000

    sec features

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+//global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: "Too many requests, please try again later.",
+});
+
+//security middleware
+app.use("/api", limiter);
+app.use(helmet());
 
 //logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -14,6 +31,10 @@ if (process.env.NODE_ENV === "development") {
 //body parser middleware
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// protect against HTTP Parameter Pollution
+app.use(hpp());
 
 //global error handler
 app.use((err, req, res, next) => {
@@ -24,6 +45,24 @@ app.use((err, req, res, next) => {
     ...PORT(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
+
+//cors middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  })
+);
 
 //api routes
 
